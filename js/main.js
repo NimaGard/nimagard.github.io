7df4ea90541be655150fcccdcc0adf9e9dd065c4
NimaGard <ajam.nima@gmail.com>
2026-03-11 16:03:39 -0400

    Personal site: about, blog, talks, experience

--- js/main.js
+++ js/main.js
@@ -18,8 +18,8 @@ themeToggle.addEventListener('click', () => {
 // Scroll Animations (Intersection Observer)
 // ========================================
 const observerOptions = {
-    threshold: 0.1,
-    rootMargin: '0px 0px -50px 0px'
+    threshold: 0,
+    rootMargin: '0px 0px -30px 0px'
 };
 
 const observer = new IntersectionObserver((entries) => {
